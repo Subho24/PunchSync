a5eb0f97449e13bd655e3e14f0b2eeafd5cc48d7
Subhojit Saha <subhojitan@gmail.com>
2024-12-03 14:03:36 +0100

    Initial Commit

---
+++ PunchSync/PunchSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  PunchSyncApp.swift
+//  PunchSync
+//
+//  Created by Subhojit Saha on 2024-12-03.
+//
+
+import SwiftUI
+
+@main
+struct PunchSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
