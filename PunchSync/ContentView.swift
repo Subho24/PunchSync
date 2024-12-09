ba84a47e179620377a0f9973012c43a0661f4f95
arlindaislami <arlindaaislamii@gmail.com>
2024-12-09 14:36:54 +0100

    I created the tabBar

--- PunchSync/ContentView.swift
+++ PunchSync/ContentView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+      
+        TabView {
+            Tab("Dashboard", systemImage: ""){
+                Text("Hej Admin")
+            }
+            Tab("Check In / Out", systemImage: ""){
+                Text("Hej User")
+            }
+            Tab("More", systemImage: ""){
+                Text("More as Admin")
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
 }
+ 
