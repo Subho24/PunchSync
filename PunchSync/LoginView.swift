d89788c85dcff1ae2230bd0a895d9babfeadbd67
Kattis91 <katya.svedmark@gmail.com>
2024-12-14 13:35:05 +0100

    Add a new icon and fix the UI for the Login page

--- PunchSync/LoginView.swift
+++ PunchSync/LoginView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State var username = ""
+    @State var password = ""
+    
     var body: some View {
-        Text("Login")
+        
+        VStack {
+            
+            Image("Icon")
+                .resizable()
+                .frame(width: 180, height: 180)
+                .padding(.leading, 25)
+            
+            Text("Log In to Your Account")
+                .font(.title2)
+                .padding(.vertical, 50)
+            
+            TextField("Username or Email", text: $username)
+                .frame(height: 38)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.black.opacity(0.8), lineWidth: 0.5) // Border with rounded corners
+                )
+                .padding([.horizontal], 45)
+                .padding(.bottom, 10)
+            
+            TextField("Password", text: $password)
+                .frame(height: 38)
+                .textFieldStyle(PlainTextFieldStyle())
+                .padding(.horizontal)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.black.opacity(0.8), lineWidth: 0.5) // Border with rounded corners
+                )
+                .padding([.horizontal], 45)
+                .padding(.bottom, 3)
+            
+            VStack {
+                ButtonView(buttontext: "Log in")
+            }
+            .padding(.vertical, 38)
+            
+        }
+        
     }
 }
 
