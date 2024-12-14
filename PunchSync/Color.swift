986cea8c9dccabd8f281c749ae1d33d05aa10c0e
Kattis91 <katya.svedmark@gmail.com>
2024-12-14 11:25:17 +0100

    Add extension Color for the buttons

--- PunchSync/Color.swift
+++ PunchSync/Color.swift
@@ -5,4 +5,18 @@
 //  Created by Katya Durneva Svedmark on 2024-12-14.
 //
 
-import Foundation
+import SwiftUI
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        var hexNumber: UInt64 = 0
+        scanner.scanHexInt64(&hexNumber)
+        
+        let r = Double((hexNumber & 0xFF0000) >> 16) / 255
+        let g = Double((hexNumber & 0x00FF00) >> 8) / 255
+        let b = Double(hexNumber & 0x0000FF) / 255
+        
+        self.init(red: r, green: g, blue: b)
+    }
+}
