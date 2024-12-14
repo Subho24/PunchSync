b7c7ef098a22684aece739eb0a4569f2d1f652ab
Kattis91 <katya.svedmark@gmail.com>
2024-12-14 12:34:17 +0100

    Refactor the code by using button view

--- PunchSync/UnloggedView.swift
+++ PunchSync/UnloggedView.swift
@@ -24,68 +24,15 @@ struct UnloggedView: View {
                     .font(.title3)
                 
                 NavigationLink(destination: LoginView()) {
-                    Text("Log in")
-                        .foregroundStyle(Color.white)
-                        .padding(.vertical, 12)
-                        .frame(minWidth: 190.0)
-                        .background(
-                            LinearGradient(
-                                gradient: Gradient(colors: [
-                                    Color(hex: "FE7E65"), // Start color
-                                    Color(hex: "E58D35"), // Middle color
-                                    Color(hex: "FD9709")  // End color
-                                ]),
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
-                            )
-                        )
-                        .cornerRadius(12)
-                        .shadow(radius: 5)
-                        .cornerRadius(12)
-                        .padding(.top, 30)
+                    ButtonView(buttontext: "Log in")
                 }
                 
                 NavigationLink(destination: SignUpAsCompanyView()) {
-                    Text("Sign Up as Company")
-                        .foregroundStyle(Color.white)
-                        .padding(.vertical, 12)
-                        .frame(minWidth: 190.0)
-                        .background(
-                            LinearGradient(
-                                gradient: Gradient(colors: [
-                                    Color(hex: "FE7E65"), // Start color
-                                    Color(hex: "E58D35"), // Middle color
-                                    Color(hex: "FD9709")  // End color
-                                ]),
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
-                            )
-                        )
-                        .cornerRadius(12)
-                        .shadow(radius: 5)
-                        .cornerRadius(12)
-                        .padding()
+                    ButtonView(buttontext: "Sign Up as Company")
                }
                 
                 NavigationLink(destination: SignUpAsEmployerView()) {
-                    Text("Sign Up as Employee")
-                        .foregroundStyle(Color.white)
-                        .padding(.vertical, 12)
-                        .frame(minWidth: 190.0)
-                        .background(
-                            LinearGradient(
-                                gradient: Gradient(colors: [
-                                    Color(hex: "FE7E65"), // Start color
-                                    Color(hex: "E58D35"), // Middle color
-                                    Color(hex: "FD9709")  // End color
-                                ]),
-                                startPoint: .topLeading,
-                                endPoint: .bottomTrailing
-                            )
-                        )
-                        .cornerRadius(12)
-                        .shadow(radius: 5)
-                        .cornerRadius(12)
+                    ButtonView(buttontext: "Sign Up as Employer")
                 }
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
