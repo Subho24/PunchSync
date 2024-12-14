f5c096f530530055dbb089f3946ccd661e1c542e
Kattis91 <katya.svedmark@gmail.com>
2024-12-14 11:24:31 +0100

    Create new views. Unlogged view with navigation to sign up and login views.

--- PunchSync/SignUpAsCompanyView.swift
+++ PunchSync/SignUpAsCompanyView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct SignUpAsCompanyView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Sign up as company")
     }
 }
 
--- PunchSync/UnloggedView.swift
+++ PunchSync/UnloggedView.swift
@@ -8,17 +8,88 @@
 import SwiftUI
 
 struct UnloggedView: View {
+    
     var body: some View {
         
-        VStack {
-            Text("PunchSync")
-                .font(.largeTitle)
-                .padding()
-            Text("Login")
-                .padding()
-            Text("Sign Up as Company")
-                .padding()
-            Text("Sign Up as Employee")
+        NavigationStack {
+            
+            VStack {
+                
+                Text("PunchSync")
+                    .font(.largeTitle)
+                    .padding()
+                Text("Manage work schedules and track your team's performance effortlessly")
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .font(.title3)
+                
+                NavigationLink(destination: LoginView()) {
+                    Text("Log in")
+                        .foregroundStyle(Color.white)
+                        .padding(.vertical, 12)
+                        .frame(minWidth: 190.0)
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    Color(hex: "FE7E65"), // Start color
+                                    Color(hex: "E58D35"), // Middle color
+                                    Color(hex: "FD9709")  // End color
+                                ]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .cornerRadius(12)
+                        .shadow(radius: 5)
+                        .cornerRadius(12)
+                        .padding(.top, 30)
+                }
+                
+                NavigationLink(destination: SignUpAsCompanyView()) {
+                    Text("Sign Up as Company")
+                        .foregroundStyle(Color.white)
+                        .padding(.vertical, 12)
+                        .frame(minWidth: 190.0)
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    Color(hex: "FE7E65"), // Start color
+                                    Color(hex: "E58D35"), // Middle color
+                                    Color(hex: "FD9709")  // End color
+                                ]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .cornerRadius(12)
+                        .shadow(radius: 5)
+                        .cornerRadius(12)
+                        .padding()
+               }
+                
+                NavigationLink(destination: SignUpAsEmployerView()) {
+                    Text("Sign Up as Employee")
+                        .foregroundStyle(Color.white)
+                        .padding(.vertical, 12)
+                        .frame(minWidth: 190.0)
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    Color(hex: "FE7E65"), // Start color
+                                    Color(hex: "E58D35"), // Middle color
+                                    Color(hex: "FD9709")  // End color
+                                ]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .cornerRadius(12)
+                        .shadow(radius: 5)
+                        .cornerRadius(12)
+                }
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.background)
         }
     }
 }
