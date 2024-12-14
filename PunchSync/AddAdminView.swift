2c31995704ee8b6906f68d4c1ad0e4a47a4e0456
Kattis91 <katya.svedmark@gmail.com>
2024-12-14 13:45:40 +0100

    Create an add admin view

---
+++ PunchSync/AddAdminView.swift
@@ -0,0 +1,18 @@
+//
+//  AddAdminView.swift
+//  PunchSync
+//
+//  Created by Katya Durneva Svedmark on 2024-12-14.
+//
+
+import SwiftUI
+
+struct AddAdminView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddAdminView()
+}
