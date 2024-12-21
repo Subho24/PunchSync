5e6d08e350950f545a529272468cd5c14e4e814b
Subhojit Saha <subhojitan@gmail.com>
2024-12-21 01:18:12 +0100

    fixed user check-in view and logic

--- PunchSync/Views/TabViews/Check_in_out.swift
+++ PunchSync/Views/TabViews/Check_in_out.swift
@@ -1,15 +1,13 @@
-//
-//  Check_in_out.swift
-//  PunchSync
-//
-//  Created by Subhojit Saha on 2024-12-19.
-//
-
 import SwiftUI
+import Firebase
 
 struct Check_in_out: View {
     
     @State private var userInput: String = ""
+    @State private var showButtons = false
+    
+    
+    var punchRecords: [String: Any] = [:]
     
     let numberPad: [[String]] = [
         ["1", "2", "3"],
@@ -29,65 +27,104 @@ struct Check_in_out: View {
         }
     }
     
+    func checkInUser(_ personalNummer: String) {
+        var ref: DatabaseReference!
+        
+        ref = Database.database().reference()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        let currentDateTimeString = dateFormatter.string(from: Date())
+        
+        print("\(userInput) \(currentDateTimeString)")
+        
+        let punchData: [String: String] = [
+            "checkInTime": currentDateTimeString,
+            "checkOutTime": "",
+            "breakStartTime":  "",
+            "breakEndTime": ""
+        ]
+        
+        ref.child("punch_records").child(personalNummer).setValue(punchData)
+    }
     
     var body: some View {
-        VStack(spacing: 10) {
-            Text("Perosnal Number")
-                .font(.title)
-                .padding(30)
-            Text(userInput.isEmpty ? "YYMMDD-XXXX" : userInput)
-                .font(.title)
-                .opacity(userInput.isEmpty ? 0.3 : 1)
-            Rectangle()
-                .frame(width: 300, height: 1)
-                .foregroundColor(.black)
-                .padding(.bottom, 30)
-            ForEach(numberPad, id: \.self) { row in
-                HStack(spacing: 10) {
-                    ForEach(row, id: \.self) { item in
-                        Button(action: {
-                            handleInput(item)
-                        }) {
-                            Text(item)
-                                .font(.title)
-                                .frame(width: 70, height: 70)
-                                .background(Color.gray.opacity(item.isEmpty ? 0 : 0.4))
-                                .foregroundColor(.black)
-                                .cornerRadius(10)
+        ZStack {
+            VStack(spacing: 10) {
+                Text("Personal Number")
+                    .font(.title)
+                    .padding(30)
+                Text(userInput.isEmpty ? "YYMMDD-XXXX" : userInput)
+                    .font(.title)
+                    .opacity(userInput.isEmpty ? 0.3 : 1)
+                Rectangle()
+                    .frame(width: 300, height: 1)
+                    .foregroundColor(.black)
+                    .padding(.bottom, 30)
+                ForEach(numberPad, id: \.self) { row in
+                    HStack(spacing: 10) {
+                        ForEach(row, id: \.self) { item in
+                            Button(action: {
+                                handleInput(item)
+                            }) {
+                                Text(item)
+                                    .font(.title)
+                                    .frame(width: 70, height: 70)
+                                    .background(Color.gray.opacity(item.isEmpty ? 0 : 0.4))
+                                    .foregroundColor(.black)
+                                    .cornerRadius(10)
+                            }
+                            .disabled(item.isEmpty) // Disable empty buttons
                         }
-                        .disabled(item.isEmpty) // Disable empty buttons
+                    }
+                }
+                
+                Button(action: {
+                    // Your button action here
+                    if !userInput.isEmpty && ValidationUtils.isValidPersonalNumber(userInput) {
+                        showButtons = true
+                        checkInUser(userInput)
+                    }
+                    
+                }) {
+                    ButtonView(buttontext: "Next")
+                        .font(.title)
+                        .foregroundColor(.white) // Text color
+                        .padding()
+                        .frame(width: 200, height: 50)
+                        .background(
+                            LinearGradient(
+                                gradient: Gradient(colors: [
+                                    Color(red: 254/255, green: 126/255, blue: 101/255), // RGB(254, 126, 101)
+                                    Color(red: 229/255, green: 141/255, blue: 53/255), // RGB(229, 141, 53)
+                                    Color(red: 253/255, green: 151/255, blue: 9/255)   // RGB(253, 151, 9)
+                                ]),
+                                startPoint: .topLeading,
+                                endPoint: .bottomTrailing
+                            )
+                        )
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
+                    
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
+                
+                if showButtons{
+                    Button(action: {
+                        // checkin / out employee
+                    }) {
+                        ButtonView(buttontext: "check-In")
                     }
                 }
             }
-            Button(action: {
-                // Your button action here
-            }) {
-                Text("Check In/Out")
-                    .font(.title)
-                    .foregroundColor(.white) // Text color
-                    .padding()
-                    .frame(width: 200, height: 50)
-                    .background(
-                        LinearGradient(
-                            gradient: Gradient(colors: [
-                                Color(red: 254/255, green: 126/255, blue: 101/255), // RGB(254, 126, 101)
-                                Color(red: 229/255, green: 141/255, blue: 53/255), // RGB(229, 141, 53)
-                                Color(red: 253/255, green: 151/255, blue: 9/255)   // RGB(253, 151, 9)
-                            ]),
-                            startPoint: .topLeading,
-                            endPoint: .bottomTrailing
-                        )
-                    )
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-            }
-            .padding()
         }
-        .background(Color.white)
     }
-
 }
 
-#Preview {
-    Check_in_out()
+struct Check_in_out_Previews: PreviewProvider {
+    static var previews: some View {
+        Check_in_out()
+    }
 }
