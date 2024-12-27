4167741a0553cf0075b6b66b2faa332f7e2b7e21
Kattis91 <katya.svedmark@gmail.com>
2024-12-27 11:29:57 +0100

    Add some UI to check if it works to load admin data

--- PunchSync/Views/TabViews/EmployerView.swift
+++ PunchSync/Views/TabViews/EmployerView.swift
@@ -6,10 +6,48 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseAuth
 
 struct EmployerView: View {
+    
+    @State var searchField: String = ""
+    @StateObject private var adminData = AdminData()
+    @State var punchsyncfb = PunchSyncFB()
+    
     var body: some View {
-        Text("Employer")
+        
+        HStack {
+            Circle()
+                .fill(Color.white)
+                .frame(width: 80, height: 80)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .shadow(radius: 5)
+                .padding(.bottom, 5)
+            
+            VStack {
+                // Display admin's name
+                Text("Admin: \(adminData.fullName)")
+                    .font(.headline)
+            }
+            .padding()
+            .task {
+                // Load admin data when the view appears
+                await punchsyncfb.loadAdminData(adminData: adminData)
+            }
+            
+            Spacer()
+        }
+        .padding()
+        .padding(.leading, 30)
+        .frame(maxWidth: .infinity)
+        .background(Color(hex: "ECE9D4"))
+        
+        VStack {
+            TextFieldView(placeholder: "Search employees", text: $searchField, isSecure: false, systemName: "magnifyingglass")
+            Spacer()
+        }
+        .padding(.top, 20)
     }
 }
 
