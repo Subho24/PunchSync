0e8147a734edff71ec3fd8a6cc36e5c77a2032c5
Kattis91 <katya.svedmark@gmail.com>
2025-01-05 21:16:32 +0100

    Make sure that add admin form only appears when admin's password is correct

--- PunchSync/Views/Authentication/AddAdminView.swift
+++ PunchSync/Views/Authentication/AddAdminView.swift
@@ -29,86 +29,140 @@ struct AddAdminView: View {
     @State var showAlert = false
     
     @State private var currentAdminPassword: String = ""
+    @State var showAdminForm = false
+    @State private var isAuthenticating = false
     
     var body: some View {
         
-        VStack {
-            
-            Image("Icon")
-                .resizable()
-                .frame(width: 120, height: 120)
-                .padding(.leading, 25)
-            
-            Text("Add Admin")
-                .font(.title2)
-                .padding(.vertical, 20)
-            
-            TextFieldView(placeholder: "Company Code", text: $yourcompanyID, isSecure: false, systemName: "number")
-                .disabled(true)
-            
-            TextFieldView(placeholder: "Full Name", text: $fullName, isSecure: false, systemName: "person", onChange: { errorMessage = ""})
-                
-            TextFieldView(placeholder: "Email", text: $email, isSecure: false, systemName: "envelope", onChange: { errorMessage = ""})
-            
-            TextFieldView(placeholder: "Password", text: $password, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
-            
-            TextFieldView(placeholder: "Confirm Password", text: $confirmPassword, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
-            
-            TextFieldView(placeholder: "Your own password", text: $currentAdminPassword, isSecure: true , systemName: "lock", onChange: { errorMessage = ""})
-            
-            ErrorMessageView(errorMessage: errorMessage)
-            
+        ScrollView {
             VStack {
-                Button(action: {
+                
+                VStack {
                     
-                    // First get the current logged-in admin
-                    guard let currentAdmin = Auth.auth().currentUser else {
-                        errorMessage = "No admin is currently logged in"
-                        return
-                    }
+                    Image("Icon")
+                        .resizable()
+                        .frame(width: 120, height: 120)
+                        .padding(.leading, 25)
                     
-                    // Validate admin password
-                    guard !currentAdminPassword.isEmpty else {
-                        errorMessage = "Please enter your admin password"
-                        return
-                    }
+                    TextFieldView(placeholder: "Your own password", text: $currentAdminPassword, isSecure: true , systemName: "lock", onChange: { errorMessage = ""})
+                        .padding(.top, 35)
+                        .disabled(showAdminForm)
                     
-                    if let validationError = ValidationUtils.validateRegisterInputs(fullName: fullName, email: email, password: password, confirmPassword: confirmPassword, companyCode: yourcompanyID) {
-                        errorMessage = validationError
-                    } else {
-                        punchsyncfb.createNewAdmin(email: email, password: password, fullName: fullName, yourcompanyID: yourcompanyID, currentAdmin: currentAdmin,  adminPassword: currentAdminPassword) { firebaseError in
-                            if let error = firebaseError {
-                                errorMessage = error
-                            } else {
-                                errorMessage = ""
-                                showAlert = true
-                                addedAdminName = fullName
-                                // Clear form fields only on success
-                                email = ""
-                                password = ""
-                                confirmPassword = ""
-                                fullName = ""
-                                currentAdminPassword = ""
+                    if !showAdminForm {
+                        
+                        ErrorMessageView(errorMessage: errorMessage)
+                        
+                        Button(action: {
+                            guard let currentAdmin = Auth.auth().currentUser else {
+                                errorMessage = "No admin is currently logged in"
+                                return
                             }
+                            
+                            guard !currentAdminPassword.isEmpty else {
+                                errorMessage = "Please enter your admin password"
+                                return
+                            }
+                            
+                            errorMessage = "" // Töm eventuella tidigare felmeddelanden
+                            isAuthenticating = true // Markera att autentisering pågår
+                            
+                            let currentAdminEmail = currentAdmin.email ?? ""
+                            let adminPassword = currentAdminPassword
+                            
+                            let credential = EmailAuthProvider.credential(withEmail: currentAdminEmail, password: adminPassword)
+                            
+                            currentAdmin.reauthenticate(with: credential) { _, error in
+                                DispatchQueue.main.async {
+                                    isAuthenticating = false // Autentisering är klar
+                                    
+                                    if let error = error {
+                                        // Fel vid lösenordsverifiering
+                                        errorMessage = "Current admin password is incorrect"
+                                    } else {
+                                        // Lösenord verifierat korrekt
+                                        errorMessage = ""
+                                        showAdminForm = true
+                                    }
+                                }
+                            }
+                        }) {
+                            ButtonView(buttontext: isAuthenticating ? "Authenticating..." : "Enter")
                         }
                     }
-                }) {
-                    ButtonView(buttontext: "Add")
+                }
+                
+                
+                if showAdminForm {
+                    Text("Add Admin")
+                        .font(.title2)
+                        .padding(.vertical, 20)
+                    
+                    TextFieldView(placeholder: "Company Code", text: $yourcompanyID, isSecure: false, systemName: "number")
+                        .disabled(true)
+                    
+                    TextFieldView(placeholder: "Full Name", text: $fullName, isSecure: false, systemName: "person", onChange: { errorMessage = ""})
+                    
+                    TextFieldView(placeholder: "Email", text: $email, isSecure: false, systemName: "envelope", onChange: { errorMessage = ""})
+                    
+                    TextFieldView(placeholder: "Password", text: $password, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
+                    
+                    TextFieldView(placeholder: "Confirm Password", text: $confirmPassword, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
+                    
+                    ErrorMessageView(errorMessage: errorMessage)
+                    
+                    VStack {
+                        Button(action: {
+                            
+                            // First get the current logged-in admin
+                            guard let currentAdmin = Auth.auth().currentUser else {
+                                errorMessage = "No admin is currently logged in"
+                                return
+                            }
+                            
+                            // Validate admin password
+                            guard !currentAdminPassword.isEmpty else {
+                                errorMessage = "Please enter your admin password"
+                                return
+                            }
+                            
+                            if let validationError = ValidationUtils.validateRegisterInputs(fullName: fullName, email: email, password: password, confirmPassword: confirmPassword, companyCode: yourcompanyID) {
+                                errorMessage = validationError
+                            } else {
+                                punchsyncfb.createNewAdmin(email: email, password: password, fullName: fullName, yourcompanyID: yourcompanyID, currentAdmin: currentAdmin,  adminPassword: currentAdminPassword) { firebaseError in
+                                    if let error = firebaseError {
+                                        errorMessage = error
+                                    } else {
+                                        errorMessage = ""
+                                        showAlert = true
+                                        addedAdminName = fullName
+                                        // Clear form fields only on success
+                                        email = ""
+                                        password = ""
+                                        confirmPassword = ""
+                                        fullName = ""
+                                        currentAdminPassword = ""
+                                    }
+                                }
+                            }
+                        }) {
+                            ButtonView(buttontext: "Add")
+                        }
+                    }
+                    .alert("New Admin Created", isPresented: $showAlert) {
+                        Button("OK", role: .cancel) {
+                            // Optional: Clear the form fields after successful creation
+                            email = ""
+                            password = ""
+                            confirmPassword = ""
+                            fullName = ""
+                            dismiss()
+                        }
+                    } message: {
+                        Text("\(addedAdminName) has been added as an admin.")
+                    }
+                    .padding(.vertical, 10)
                 }
             }
-            .alert("New Admin Created", isPresented: $showAlert) {
-                Button("OK", role: .cancel) {
-                    // Optional: Clear the form fields after successful creation
-                    email = ""
-                    password = ""
-                    confirmPassword = ""
-                    fullName = ""
-                    dismiss()
-                }
-            } message: {
-                Text("\(addedAdminName) has been added as an admin.")
-            }
-            .padding(.vertical, 10)
         }
     }
 }
