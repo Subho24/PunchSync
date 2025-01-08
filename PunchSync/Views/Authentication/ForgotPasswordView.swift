3b4f04226d5ae3ddf8f263e919120039cbe3514a
Kattis91 <katya.svedmark@gmail.com>
2025-01-08 13:22:50 +0100

    Update the style so there is only one button on the row

---
+++ PunchSync/Views/Authentication/ForgotPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ForgotPasswordView.swift
+//  PunchSync
+//
+//  Created by Katya Durneva Svedmark on 2025-01-08.
+//
+
+import SwiftUI
+
+struct ForgotPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ForgotPasswordView()
+}
