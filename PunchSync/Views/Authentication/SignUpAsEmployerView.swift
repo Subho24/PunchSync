0b0fa9ea8ef7ae9ed723b4ac4f605fd3a05ea654
Kattis91 <katya.svedmark@gmail.com>
2025-01-09 13:24:10 +0100

    Add a ScrollView to work on smaller devices

--- PunchSync/Views/Authentication/SignUpAsEmployerView.swift
+++ PunchSync/Views/Authentication/SignUpAsEmployerView.swift
@@ -26,56 +26,57 @@ struct SignUpAsEmployerView: View {
     
     var body: some View {
         
-        VStack {
-            
-            Image("Icon")
-                .resizable()
-                .frame(width: 180, height: 180)
-                .padding(.leading, 25)
-            
-            Text("Sign Up as Employee")
-                .font(.title2)
-                .padding(.vertical, 20)
-            
-            TextFieldView(placeholder: "Full Name", text: $fullName, isSecure: false, systemName: "person", onChange: {
-                errorMessage = ""
-            })
-            
-            TextFieldView(placeholder: "Personal Number (12 numbers)", text: $personalNumber, isSecure: false, systemName: "lock", onChange: {
-                personalNumber = ValidationUtils.formatPersonalNumber(personalNumber);
-                errorMessage = ""
-            })
-            
-            TextFieldView(placeholder: "Email", text: $email, isSecure: false, systemName: "envelope", onChange: { errorMessage = ""})
-            
-            TextFieldView(placeholder: "Password", text: $password, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
-            
-            TextFieldView(placeholder: "Confirm Password", text: $confirmPassword, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
-            
-            TextFieldView(placeholder: "Company Code", text: $companyCode, isSecure: false, systemName: "number", onChange: { errorMessage = ""})
-            
-            ErrorMessageView(errorMessage: errorMessage)
-            
+        ScrollView {
             VStack {
-                Button(action: {
-                    if let validationError = ValidationUtils.validateRegisterInputs(fullName: fullName, email: email, password: password, confirmPassword: confirmPassword, companyCode: companyCode, personalNumber: personalNumber) {
-                        errorMessage = validationError
-                    } else {
-                        punchsyncfb.saveUserData(fullName: fullName, personalNumber: personalNumber, email: email, companyCode: companyCode) { success, error in
-                            if let error = error {
-                                self.errorMessage = error
-                            } else if success {
-                                punchsyncfb.userRegister(email: email, password: password) { firebaseError in
-                                    errorMessage = firebaseError ?? "" // Default to empty string if no Firebase error
+                Image("Icon")
+                    .resizable()
+                    .frame(width: 180, height: 180)
+                    .padding(.leading, 25)
+                
+                Text("Sign Up as Employee")
+                    .font(.title2)
+                    .padding(.vertical, 20)
+                
+                TextFieldView(placeholder: "Full Name", text: $fullName, isSecure: false, systemName: "person", onChange: {
+                    errorMessage = ""
+                })
+                
+                TextFieldView(placeholder: "Personal Number (12 numbers)", text: $personalNumber, isSecure: false, systemName: "lock", onChange: {
+                    personalNumber = ValidationUtils.formatPersonalNumber(personalNumber);
+                    errorMessage = ""
+                })
+                
+                TextFieldView(placeholder: "Email", text: $email, isSecure: false, systemName: "envelope", onChange: { errorMessage = ""})
+                
+                TextFieldView(placeholder: "Password", text: $password, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
+                
+                TextFieldView(placeholder: "Confirm Password", text: $confirmPassword, isSecure: true, systemName: "lock", onChange: { errorMessage = ""})
+                
+                TextFieldView(placeholder: "Company Code", text: $companyCode, isSecure: false, systemName: "number", onChange: { errorMessage = ""})
+                
+                ErrorMessageView(errorMessage: errorMessage)
+                
+                VStack {
+                    Button(action: {
+                        if let validationError = ValidationUtils.validateRegisterInputs(fullName: fullName, email: email, password: password, confirmPassword: confirmPassword, companyCode: companyCode, personalNumber: personalNumber) {
+                            errorMessage = validationError
+                        } else {
+                            punchsyncfb.saveUserData(fullName: fullName, personalNumber: personalNumber, email: email, companyCode: companyCode) { success, error in
+                                if let error = error {
+                                    self.errorMessage = error
+                                } else if success {
+                                    punchsyncfb.userRegister(email: email, password: password) { firebaseError in
+                                        errorMessage = firebaseError ?? "" // Default to empty string if no Firebase error
+                                    }
                                 }
                             }
                         }
+                    }) {
+                        ButtonView(buttontext: "Sign Up")
                     }
-                }) {
-                    ButtonView(buttontext: "Sign Up")
                 }
+                .padding(.vertical, 10)
             }
-            .padding(.vertical, 10)
         }
     }
 }
