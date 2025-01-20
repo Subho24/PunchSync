5e13e02e02e7691b59bf7f88abbce0fe1ef2c0dd
Kattis91 <katya.svedmark@gmail.com>
2025-01-20 08:53:43 +0100

    Create a LeaveRequest model

--- PunchSync/Models/LeaveRequestModel.swift
+++ PunchSync/Models/LeaveRequestModel.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+
+struct LeaveRequest: Identifiable {
+    var id: String
+    var title: String
+    var requestType: String
+    var description: String
+    var startDate: Date
+    var endDate: Date
+}
+
+extension LeaveRequest {
+    func toDictionary() -> [String: Any] {
+        return [
+            "title": title,
+            "requestType": requestType,
+            "description": description,
+            "startDate": startDate.timeIntervalSince1970,
+            "endDate": endDate.timeIntervalSince1970
+        ]
+    }
+}
